1b1b179fb35ae4c2fb1127326ffa3bc37ebb6d6f
bhojrajmishra <bhojrajmishra4@gmail.com>
2024-11-11 16:53:59 +0545

    feat: add UserDatabase

--- app/src/main/java/com/example/userlist/data/UserDao.kt
+++ app/src/main/java/com/example/userlist/data/UserDao.kt
@@ -1,4 +1,22 @@
 package com.example.userlist.data
 
-interface Dao {
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.userlist.model.UserList
+
+interface UserDao {
+    // Insert a user into the database
+    @Insert (onConflict = OnConflictStrategy.REPLACE)
+    suspend fun addUser(user: UserList)
+    // Retrieve all users from the database
+    @Query("SELECT * FROM users")
+    suspend fun getAllUsers(): List<UserList>
+    // Retrieve a specific user from the database
+    @Query("SELECT * FROM users WHERE id = :userId")
+    suspend fun getUser(userId: Int): UserList
+    // Delete all users from the database
+    @Query("DELETE FROM users")
+    suspend fun deleteAllUsers()
+
 }
